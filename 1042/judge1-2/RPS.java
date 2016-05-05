fbf6c89e339b2c754936b5009e2f987d3efb05c3
Yuer <passerbyid@gmail.com>
2016-05-05 15:39:35 +0800

    Add comment.

--- 1042/judge1-2/RPS.java
+++ 1042/judge1-2/RPS.java
@@ -3,23 +3,31 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class RPS extends JFrame implements ActionListener {
+    // 猜拳遊戲玩家列表（寫死為兩位）
     private static RPS[] players = new RPS[2];
+    // 每位玩家的出拳狀態
     private byte status;
+    // 顯示結果欄
     private JLabel screen;
+    // 出拳選擇按鈕們
     private JButton[] beats;
 
+    // 設定玩家
     public static void setPlayer(RPS p1, RPS p2) {
         players[0] = p1;
         players[1] = p2;
         initStatus();
     }
 
+    // 重置玩家出拳狀態
     private static void initStatus() {
         players[0].status = 0;
         players[1].status = 0;
     }
 
+    // 確認玩家出拳狀態
     public static void checkStatus() {
+        // 雙方皆出拳，才開始判斷結果
         if (players[0].status < 0 || players[1].status < 0) {
             setText("Something Wrong...");
         } else if (players[0].status <= 0 && players[1].status <= 0) {
@@ -35,6 +43,7 @@ public class RPS extends JFrame implements ActionListener {
         }
     }
 
+    // 設定每位玩家的狀態欄
     private static void setText(String s) {
         players[0].screen.setText(s);
         players[1].screen.setText(s);
@@ -42,6 +51,7 @@ public class RPS extends JFrame implements ActionListener {
         players[1].screen.setHorizontalAlignment(JTextField.CENTER);
     }
 
+    // 判斷比賽結果
     private static void fight() {
         if (players[0].status == players[1].status) {
             setText("和局");
@@ -58,18 +68,22 @@ public class RPS extends JFrame implements ActionListener {
                 setText("P2 贏了");
             }
         }
+        // 每次比完，重置狀態
         initStatus();
     }
 
     private RPS(String playerName) {
+        // 設定介面
         setTitle("Player " + playerName);
         setSize(200,400);
         setLayout( new GridLayout(4, 1) );
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // 設定狀態欄
         screen = new JLabel();
         add(screen);
 
+        // 設定出拳選擇鈕
         beats = new JButton[3];
         beats[0] = new JButton("剪刀");
         beats[1] = new JButton("石頭");
@@ -79,10 +93,13 @@ public class RPS extends JFrame implements ActionListener {
             add(beats[i]);
         }
 
+        // Show the Frame
         this.setVisible(true);
     }
 
+    // 設定觸發事件
     public void actionPerformed(ActionEvent e) {
+        // 取得玩家出什麼拳，並設定之
         String cmd = e.getActionCommand();
         if ( cmd.equals("剪刀") ) {
             status = 1;
@@ -91,6 +108,7 @@ public class RPS extends JFrame implements ActionListener {
         } else if ( cmd.equals("布") ) {
             status = 3;
         }
+        // 每次出拳，都確認彼此狀態
         checkStatus();
     }
 
@@ -98,3 +116,4 @@ public class RPS extends JFrame implements ActionListener {
         setPlayer(new RPS("p1"), new RPS("p2"));
     }
 }
+
